d21022cab681c0f5ce039e12ebc01f377251297d
Ketil T. <ketil.trollvang@gmail.com>
2024-10-04 14:00:58 +0200

    Update GPSDataConverter.java
    Samme logikk, bare mer konsis

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -4,41 +4,15 @@ import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSDataConverter {
+	private static int TIME_STARTINDEX = 11;
+	
+	public static int toSeconds(String timestr) {
+	    String[] tid = timestr.substring(TIME_STARTINDEX, TIME_STARTINDEX + 8).split(":");
 
-    
-    private static int TIME_STARTINDEX = 11; 
+	    return Integer.parseInt(tid[0]) * 3600 + Integer.parseInt(tid[1]) * 60 + Integer.parseInt(tid[2]);
+	}
 
-    public static int toSeconds(String timestr) {
-        
-          String tidsDel = timestr.substring(TIME_STARTINDEX, TIME_STARTINDEX + 8);
-            
-            String[] tidsDeler = tidsDel.split(":");
-    
-          
-          int hrs = Integer.parseInt(tidsDeler[0]);
-          int min = Integer.parseInt(tidsDeler[1]);
-          int sec = Integer.parseInt(tidsDeler[2]);
-          
-          int totalSec = (hrs * 3600) + (min * 60) + sec;
-          
-          
-          return totalSec;
-
-        
-    }
-
-    public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
-        
-        int time = toSeconds(timeStr);
-        double latitude = Double.parseDouble(latitudeStr);
-        double longitude = Double.parseDouble(longitudeStr);
-        double elevation = Double.parseDouble(elevationStr);
-        
-        
-        GPSPoint gpsPoint  = new GPSPoint(time, latitude, longitude, elevation);
-        
-        return gpsPoint ;
-    
-    }
-    
+	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
+		return new GPSPoint(toSeconds(timeStr), Double.parseDouble(latitudeStr), Double.parseDouble(longitudeStr), Double.parseDouble(elevationStr));
+	}
 }
