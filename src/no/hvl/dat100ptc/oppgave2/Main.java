991bedd27cfd00bb0e4dfc0e97a43b62f3cfdc92
Ketil T. <ketil.trollvang@gmail.com>
2024-10-04 17:31:12 +0200

    Løst oppgave 3
    Løst oppgave 3

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -4,15 +4,14 @@ import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
 		GPSPoint point1 = new GPSPoint(3600, 60.5131, 5.2331, 50.0);
 		GPSPoint point2 = new GPSPoint(7400, 54.2221, 7.3371, 70.0);
 		GPSData gpsData = new GPSData(2);
-		
+
 		gpsData.insertGPS(point1);
 		gpsData.insertGPS(point2);
-		
+
 		gpsData.print();
 	}
 }
--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -3,113 +3,72 @@ package no.hvl.dat100ptc.oppgave3;
 import static java.lang.Math.*;
 
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
-import no.hvl.dat100ptc.TODO;
 
 public class GPSUtils {
+	private static final int R = 6371000; // jordens radius
+	private static int TEXTWIDTH = 10;
 
 	public static double findMax(double[] da) {
+		double max = da[0];
 
-		double max; 
-		
-		max = da[0];
-		
-		for (double d : da) {
-			if (d > max) {
+		for (double d : da)
+			if (d > max)
 				max = d;
-			}
-		}
-		
+
 		return max;
 	}
 
 	public static double findMin(double[] da) {
+		double min = da[0];
 
-		double min;
+		for (double d : da)
+			if (d < min)
+				min = d;
 
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
+		return min;
 	}
 
 	public static double[] getLatitudes(GPSPoint[] gpspoints) {
+		double[] lat = new double[gpspoints.length];
 
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO
+		for (int i = 0; i < lat.length; i++)
+			lat[i] = gpspoints[i].getLatitude();
+
+		return lat;
 	}
 
 	public static double[] getLongitudes(GPSPoint[] gpspoints) {
+		double[] lon = new double[gpspoints.length];
 
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
+		for (int i = 0; i < lon.length; i++)
+			lon[i] = gpspoints[i].getLongitude();
 
+		return lon;
 	}
-
-	private static final int R = 6371000; // jordens radius
 
 	public static double distance(GPSPoint gpspoint1, GPSPoint gpspoint2) {
-
-		double d;
-		double latitude1, longitude1, latitude2, longitude2;
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		return R * compute_c(compute_a(toRadians(gpspoint1.getLatitude()), toRadians(gpspoint2.getLatitude()),
+				toRadians(gpspoint2.getLatitude() - gpspoint1.getLatitude()),
+				toRadians(gpspoint2.getLongitude() - gpspoint1.getLongitude())));
 	}
-	
-	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
-	
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
 
+	private static double compute_a(double phi1, double phi2, double deltaphi, double deltadelta) {
+		return pow(sin(deltaphi / 2), 2) + cos(phi1) * cos(phi2) * pow(sin(deltadelta / 2), 2);
 	}
 
 	private static double compute_c(double a) {
-
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		
-		// TODO 
-
+		return 2 * atan2(sqrt(a), sqrt(1 - a));
 	}
 
-	
 	public static double speed(GPSPoint gpspoint1, GPSPoint gpspoint2) {
-
-		int secs;
-		double speed;
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO
-
+		return distance(gpspoint1, gpspoint2) / (gpspoint2.getTime() - gpspoint1.getTime());
 	}
 
 	public static String formatTime(int secs) {
-
-		String timestr;
-		String TIMESEP = ":";
-
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO 
-		
+		return "  " + java.time.LocalTime.ofSecondOfDay(secs).toString();
 	}
-	
-	private static int TEXTWIDTH = 10;
 
 	public static String formatDouble(double d) {
-
-		String str;
-
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO
-		
+		return String.format("%" + TEXTWIDTH + ".2f", d);
 	}
 }
