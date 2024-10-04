dddf8ea324112685d8e47ca1a85d1be2bc358c62
HanssenGLRP <66432612+186622@users.noreply.github.com>
2024-10-04 11:10:41 +0200

    Update Main.java

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -2,10 +2,16 @@ package no.hvl.dat100ptc.oppgave1;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		// TODO
-		
-	}
+    public static void main(String[] args) {
+        GPSPoint point = new GPSPoint(1, 2.0, 3.0, 5.0);
+        point.setTime(2);
+
+        System.out.println("Tidspunkt: " + point.getTime());
+        
+        
+        System.out.println(point.toString());
+    
+        
+    }
 
 }
